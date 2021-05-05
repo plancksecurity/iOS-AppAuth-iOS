d7c8a3cb85b19a57f95a5cdf66a034c8dacaa5f3
Dirk Zimmermann <dz@pep.security>
2021-05-05 11:01:06 +0200

    IOS-2791 [UIApplication sharedApplication]--

--- Source/iOS/OIDExternalUserAgentIOS.m
+++ Source/iOS/OIDExternalUserAgentIOS.m
@@ -120,7 +120,9 @@ static id<OIDSafariViewControllerFactory> __nullable gSafariViewControllerFactor
     [_presentingViewController presentViewController:safariVC animated:YES completion:nil];
     openedSafari = YES;
   } else {
+#if !SHARE_EXT
     openedSafari = [[UIApplication sharedApplication] openURL:requestURL];
+#endif
   }
 
   if (!openedSafari) {
